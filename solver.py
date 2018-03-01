0df9c983837eed7f391e14a43d8166f7d9b12853
Bogdan <serbanbogdan2008@yahoo.com>
2018-03-01 21:29:27 +0000

    final solution 34,183,099

--- solver.py
+++ solver.py
@@ -29,63 +29,93 @@ class Solver(object):
                 output_file.write(res + '\n')
 
     def get_best_ride(self, vehicle):
-        las_position = vehicle.get_last_position()
 
-        smallest_dist = 200000
-        winners = []
-        for ride in self.rides:
-            dist = utils.point_dist(las_position, ride.start_point)
-            if dist < smallest_dist:
-                winners = [ride]
-                smallest_dist = dist
-            elif dist == smallest_dist:
-                winners.append(ride)
+        winners = self.get_best_by_start_time(vehicle, self.rides)
         if len(winners) == 0:
             self.satisfied_vehicles_count += 1
             return None
 
-        max_length = 0
-        length_winners = []
-        for ride in winners:
-            if ride.length + vehicle.ride_distance <= ride.latest_finish:
-                if ride.length > max_length:
-                    length_winners = [ride]
-                    max_length = ride.length
-                elif ride.length == max_length:
-                    length_winners.append(ride)
-        if len(length_winners) == 0:
+        winners = self.get_max_by_starting_point(vehicle, winners)
+        if len(winners) == 0:
             self.satisfied_vehicles_count += 1
             return None
 
+        winners = self.get_max_by_length(vehicle, winners)
+        if len(winners) == 0:
+            self.satisfied_vehicles_count += 1
+            return None
+
+        winners = self.get_best_by_finish(vehicle, winners)
+        if len(winners):
+            winner = winners[0]
+            self.rides.remove(winner)
+            return winner
+        self.satisfied_vehicles_count += 1
+        return None
+
+    def get_max_by_starting_point(self, vehicle, search_space=None):
+        if search_space is None:
+            search_space = self.rides
+
+        las_position = vehicle.get_last_position()
+        smallest_dist = 200000
+        winners = []
+        for ride in search_space:
+            if ride.length + vehicle.ride_distance < ride.latest_finish:
+                dist = utils.point_dist(las_position, ride.start_point)
+                if dist < smallest_dist:
+                    winners = [ride]
+                    smallest_dist = dist
+                elif dist == smallest_dist:
+                    winners.append(ride)
+        return winners
+
+    def get_max_by_length(self, vehicle, winners):
+        max_length = 0
+        length_winners = []
+        for ride in winners:
+            if ride.length + vehicle.ride_distance < ride.latest_finish:
+                ratio = ride.length / self.closest_dest(ride)
+                if ratio > max_length:
+                    length_winners = [ride]
+                    max_length = ratio
+                elif ratio == max_length:
+                    length_winners.append(ride)
+        return length_winners
+
+    def closest_dest(self, ride):
+        closest = 200000
+        for x_ride in self.rides:
+            if ride.idx != x_ride.idx:
+                dist = utils.point_dist(ride.end_point, x_ride.start_point)
+                if dist < closest:
+                    closest = dist
+
+        return closest if closest != 0 else 1
+
+    def get_best_by_start_time(self, vehicle, winners):
         min_start = 200000
         start_winners = []
-        for ride in length_winners:
-            if ride.length + vehicle.ride_distance <= ride.latest_finish:
+        for ride in winners:
+            if ride.length + vehicle.ride_distance < ride.latest_finish:
                 if ride.earliest_start < min_start:
                     start_winners = [ride]
                     min_start = ride.earliest_start
                 elif ride.earliest_start == min_start:
                     start_winners.append(ride)
-        if len(start_winners) == 0:
-            self.satisfied_vehicles_count += 1
-            return None
+        return start_winners
 
+    def get_best_by_finish(self, vehicle, winners):
         min_finish = 200000
         finish_winners = []
-        for ride in start_winners:
-            if ride.length + vehicle.ride_distance <= ride.latest_finish:
+        for ride in winners:
+            if ride.length + vehicle.ride_distance < ride.latest_finish:
                 if min_finish >= ride.latest_finish:
                     finish_winners = [ride]
                     min_finish = ride.latest_finish
                 elif ride.latest_finish == min_finish:
                     finish_winners.append(ride)
-
-        if len(finish_winners):
-            winner = finish_winners[0]
-            self.rides.remove(winner)
-            return winner
-        self.satisfied_vehicles_count += 1
-        return None
+        return finish_winners
 
     def load_problem(self, file_name):
         with open(file_name, 'r') as input_file:
